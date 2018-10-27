5d4acb5973339faf409308c97b7bfa949ea73430
zhanglinyao <zhanglinyao@yunzhangfang.com>
2018-10-27 11:56:23 +0800

    Initial commit

---
+++ src/main/java/com/jba/dataflow/JbaDataflowApplication.java
@@ -0,0 +1,12 @@
+package com.jba.dataflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbaDataflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbaDataflowApplication.class, args);
+	}
+}
